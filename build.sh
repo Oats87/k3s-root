5197e7dab33b8867fe7efb035619e7200e81eded
Chris Kim <oats87g@gmail.com>
2020-05-21 14:27:10 -0700

    parallelize the build process

--- build.sh
+++ build.sh
@@ -3,11 +3,17 @@ set -x -e
 
 BUILD_ARCHS=${BUILD_ARCHS:-amd64 arm64 arm ppc64le}
 
-mkdir -p dist
-for arch in ${BUILD_ARCHS}; do
+build_and_tar()
+{
+    arch=$1
     docker build --build-arg ARCH=${arch} --tag k3s-root .
     docker run --rm k3s-root \
         tar cf - -C /usr/src ./bin ./etc > dist/k3s-root-${arch}.tar
     docker run --rm k3s-root \
         tar cf - -C /usr/src --transform s/xtables-bin/bin/ ./xtables-bin > dist/k3s-root-xtables-${arch}.tar
+}
+
+mkdir -p dist
+for arch in ${BUILD_ARCHS}; do
+    build_and_tar ${arch} &
 done
